622e4b41bed0d666d301e6b3a22f25def6ba7912
JellyPh1sh <sacha.sorgiati@gmail.com>
2024-01-06 09:02:33 +0100

    :bug: | Fix saving position on player dropped.

--- server/sv_events.lua
+++ server/sv_events.lua
@@ -1,6 +1,9 @@
 RegisterServerEvent("jph_rpgamemode:PlayerConnected")
 AddEventHandler("jph_rpgamemode:PlayerConnected", GetPlayerInfo)
 
-AddEventHandler("playerDropped", SavePlayer)
+AddEventHandler("playerDropped", function()
+    local src = source
+    SavePlayer(src)
+end)
 
 RegisterServerEvent("jph_rpgamemode:OnPlayerDeath")
